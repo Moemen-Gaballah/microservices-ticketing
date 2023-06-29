a862c14a16594436b72253e8e690181c3f3458a4
moemen gaballah <moemengaballa@gmail.com>
2023-06-29 22:45:55 +0300

    add file router to current user

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,17 @@
 import express from 'express';
 import { json } from 'body-parser';
 
+import { currentUserRouter} from "./routes/current-user";
+
 const app = express();
-app.use(json())
+app.use(json());
+
+app.use(currentUserRouter);
+
+
+app.get('/api/users/currentuser', (req, res) => {
+
+})
 
 app.listen(3000, () => {
    console.log('Listening on port 3000!');
