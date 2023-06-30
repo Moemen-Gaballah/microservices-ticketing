0754c622a6dddbc95ad077c31950a12d1d439b8f
moemen gaballah <moemengaballa@gmail.com>
2023-06-30 19:38:25 +0300

    abstract class for custom error , refactor code

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,9 +1,12 @@
-import { ValidationError } from "express-validator";
+import {ValidationError} from "express-validator";
+import {CustomError} from "./custom-error";
 
-export class RequestValidationError extends Error {
+
+export class RequestValidationError extends CustomError {
     statusCode = 400;
+
     constructor(public errors: ValidationError[]) {
-        super();
+        super("Invalid request parameters");
 
         // Only because we are extending a build in class
         Object.setPrototypeOf(this, RequestValidationError.prototype);
@@ -14,6 +17,7 @@ export class RequestValidationError extends Error {
             if (err.type === 'field') {
                 return {message: err.msg, field: err.path};
             }
+            return {message: err.msg};
         });
     }
 }
