2b8ab26fda91d86426c792f45f033ecea8c5e1b4
moemen gaballah <moemengaballa@gmail.com>
2023-07-02 10:39:35 +0300

    signin, middleware for validate request

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,9 @@
 import express, {Request, Response} from "express";
-import {body, validationResult} from "express-validator";
+import {body} from "express-validator";
 import jwt from 'jsonwebtoken';
 
+import {validateRequest} from "../middlewares/validate-request";
 import {User} from "../models/user";
-import {RequestValidationError} from "../errors/request-validation-error";
 import {BadRequestError} from "../errors/bad-request-error";
 
 const router = express.Router();
@@ -18,12 +18,8 @@ router.post('/api/users/signup', [
                 min: 4, max: 20
             }).withMessage('Password must be between 4 and 20 characters')
     ],
+    validateRequest,
     async (req: Request, res: Response) => {
-        const errors = validationResult(req);
-
-        if (!errors.isEmpty()) {
-            throw new RequestValidationError(errors.array());
-        }
 
         const {email, password} = req.body;
 
@@ -38,9 +34,11 @@ router.post('/api/users/signup', [
 
         // Generate JWT
         const userJwt = jwt.sign({
-            id: user.id,
-            email: user.email,
-        }, 'asdf');
+                id: user.id,
+                email: user.email,
+            },
+            process.env.JWT_KEY!
+        );
 
         // Store it on session object
         req.session = {
