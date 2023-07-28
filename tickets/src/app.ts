a6de76250101e7c0e635382778f1a5d8d395a363
moemen gaballah <moemengaballa@gmail.com>
2023-07-28 10:15:14 +0300

    route index tickets , test for route index, route update

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,8 +3,10 @@ import 'express-async-errors'
 import {json} from 'body-parser';
 import cookieSession from "cookie-session";
 import {currentUser, errorHandler, NotFoundError} from "@mgticketing/common";
-import {createTicketRouter } from './routes/new'
+import {createTicketRouter} from './routes/new'
 import {showTicketRouter} from "./routes/show";
+import {indexTicketRouter} from "./routes/index";
+import {updateTicketRouter} from "./routes/update";
 
 const app = express();
 app.set('trust proxy', true);
@@ -17,6 +19,8 @@ app.use(currentUser)
 
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async () => {
     throw new NotFoundError();
