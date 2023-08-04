16fbbde7658686ea433e53950addd787fa9b4251
moemen gaballah <moemengaballa@gmail.com>
2023-08-04 10:43:33 +0300

    class for publish and listen

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,22 +1,35 @@
 import nats from 'node-nats-streaming';
+import {TickeetCreatedPublisher} from "./events/tickeet-created-publisher";
+
 
 const stan = nats.connect('ticketing', 'abc', {
     url: 'http://localhost:4222',
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
     console.log('Publisher connected to NATS');
 
-    const data = JSON.stringify({
-        id: "123",
-        title: 'concert',
-        price: 20
-    });
+    const publisher = new TickeetCreatedPublisher(stan);
+    try {
+        await publisher.publish({
+            id: "123",
+            title: 'concert',
+            price: 20
+        });
+    } catch (err) {
+        console.log(err);
+    }
 
-    stan.publish('ticket:created', data, () => {
-        console.log('Event published');
-    });
 
-    
+    // const data = JSON.stringify({
+    //     id: "123",
+    //     title: 'concert',
+    //     price: 20
+    // });
+    //
+    // stan.publish('ticket:created', data, () => {
+    //     console.log('Event published');
+    // });
+
 
 });
