97ca818efd27958486311a6df946fecaab7739e2
moemen gaballah <moemengaballa@gmail.com>
2023-08-06 07:06:14 +0300

    test for create new order

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 import request from 'supertest';
 import {app} from "../../app";
+import {Order} from "../../models/order";
+import {Ticket} from "../../models/ticket";
+import {OrderStatus} from "@mgticketing/common";
 
 it('returns an error if the ticket does not exist', async () => {
     const ticketId = new mongoose.Types.ObjectId();
@@ -15,10 +18,42 @@ it('returns an error if the ticket does not exist', async () => {
 });
 
 it('returns an error if the ticket is already reserved', async () => {
+    const ticket = Ticket.build({
+        title: 'concert',
+        price:20
+    });
 
+    await ticket.save();
+    const order = Order.build({
+        ticket,
+        userId: "wadadadadw",
+        status: OrderStatus.Created,
+        expiresAt: new Date()
+    });
+
+    await order.save();
+
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({ticketId: ticket.id})
+        .expect(400);
 });
 
 it('reserves a ticket', async () => {
+    const ticket = Ticket.build({
+        title: 'concert',
+        price:20
+    });
+
+    await ticket.save();
+
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({ticketId: ticket.id})
+        .expect(201);
 
 });
 
+it.todo('emits an order created event');
