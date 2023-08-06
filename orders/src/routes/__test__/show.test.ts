16eae3c157f6a49a84bebecf9cbb69d9c832e80d
moemen gaballah <moemengaballa@gmail.com>
2023-08-06 07:25:10 +0300

    test show order

--- orders/src/routes/__test__/show.test.ts
+++ orders/src/routes/__test__/show.test.ts
@@ -27,3 +27,27 @@ it('fetches the order', async () => {
 
     expect(fetchedOrder.id).toEqual(order.id);
 });
+
+it('returns an error if one user tries to fetch another users order', async () => {
+    // create a ticket
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20
+    });
+    await ticket.save();
+
+    const user = global.signin();
+    // make a request to build an order with this ticket
+    const {body: order} = await request(app)
+        .post('/api/orders')
+        .set('Cookie', user)
+        .send({ticketId: ticket.id})
+        .expect(201);
+
+    // make request to fetch the order
+    await request(app)
+        .get(`/api/orders/${order.id}`)
+        .set('Cookie', global.signin())
+        .send()
+        .expect(401);
+});
