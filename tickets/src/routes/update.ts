5ec2fac1b7a08d55288600e55e687f1c2c774fe8
moemen gaballah <moemengaballa@gmail.com>
2023-08-07 19:00:59 +0300

    Rejecting Edits of Reserved Tickets

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -4,7 +4,7 @@ import {
     validateRequest,
     NotFoundError,
     requireAuth,
-    NotAuthorizedError
+    NotAuthorizedError, BadRequestError
 } from '@mgticketing/common';
 import {Ticket} from "../models/ticket";
 import { TicketUpdatedPublisher} from "../events/publishers/ticket-updated-publisher";
@@ -33,6 +33,10 @@ router.put('/api/tickets/:id',
 
         if (!ticket) {
             throw new NotFoundError();
+        }
+
+        if(ticket.orderId) {
+            throw new BadRequestError('Cannot edit a reserved ticket');
         }
 
         if (ticket.userId !== req.currentUser!.id) {
