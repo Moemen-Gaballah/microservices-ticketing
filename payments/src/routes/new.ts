39115b381af936f9b84bbed06c278f3786887767
moemen gaballah <moemengaballa@gmail.com>
2023-08-08 21:21:54 +0300

    Complete payment service Test

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -9,6 +9,9 @@ import {
 } from "@mgticketing/common";
 import {stripe} from "../stripe";
 import {Order} from "../models/order";
+import {Payment} from "../models/payment";
+import {PaymentCreatedPublisher} from "../events/publishers/payment-created-publisher";
+import {natsWrapper} from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -40,14 +43,26 @@ router.post(
             throw new BadRequestError('Cannot pay for an cancelled order');
         }
 
-        await stripe.charges.create({
+        const charge = await stripe.charges.create({
             currency: 'usd',
             amount: order.price * 100,
             source: token
         });
 
+        const payment = Payment.build({
+            orderId,
+            stripeId: charge.id
+        });
+
+        await payment.save();
+
+        await new PaymentCreatedPublisher(natsWrapper.client).publish({
+            id: payment.id,
+            orderId: payment.orderId
+        })
+
         res.status(201).send({
-            success: true
+            id: payment.id
         });
     }
 );
