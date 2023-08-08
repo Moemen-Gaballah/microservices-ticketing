a4cf30ba3a5227724c37991450773555fd32a941
moemen gaballah <moemengaballa@gmail.com>
2023-08-08 16:48:26 +0300

    test payment

--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -5,7 +5,7 @@ import request from "supertest";
 import jwt from 'jsonwebtoken';
 
 declare global {
-    var signin: () => string[];
+    var signin: (id?: string) => string[];
 }
 
 jest.mock('../nats-wrapper');
@@ -35,10 +35,10 @@ afterAll(async () => {
     await mongoose.connection.close();
 });
 
-global.signin = () => {
+global.signin = (id?: string) => {
     // Build  JWT payload.  { id, email }
     const payload = {
-        id: new mongoose.Types.ObjectId().toHexString(),
+        id: id || new mongoose.Types.ObjectId().toHexString(),
         email: 'test@test.com'
     };
 
@@ -50,12 +50,12 @@ global.signin = () => {
     const token = jwt.sign(payload, process.env.JWT_KEY);
 
     // Build session Object. { jwt: MY_JWT }
-    const session = { jwt: token};
+    const session = {jwt: token};
 
     // Turn that session into JSON
     const sessionJson = JSON.stringify(session);
 
-   // Take JSON and encode it as base64
+    // Take JSON and encode it as base64
     const base64 = Buffer.from(sessionJson).toString('base64');
 
     // return a string that's the cookie with the encoded data
