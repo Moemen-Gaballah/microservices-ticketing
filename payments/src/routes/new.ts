09790eafa17bc6dace97a7bc7590eb09c436bdac
moemen gaballah <moemengaballa@gmail.com>
2023-08-08 17:50:53 +0300

    integration with stripe payment

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -7,7 +7,7 @@ import {
     BadRequestError,
     NotAuthorizedError, NotFoundError, OrderStatus
 } from "@mgticketing/common";
-
+import {stripe} from "../stripe";
 import {Order} from "../models/order";
 
 const router = express.Router();
@@ -39,6 +39,12 @@ router.post(
         if (order.status === OrderStatus.Cancelled) {
             throw new BadRequestError('Cannot pay for an cancelled order');
         }
+
+        await stripe.charges.create({
+            currency: 'usd',
+            amount: order.price * 100,
+            source: token
+        });
 
         res.send({
             success: true
