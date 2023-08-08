cac52418f768e170dd8f0a449a60b5b1bd8571dd
moemen gaballah <moemengaballa@gmail.com>
2023-08-08 07:11:49 +0300

    expiration-queue

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,13 +1,24 @@
 import {Listener, OrderCreatedEvent, Subjects} from "@mgticketing/common";
 import {queueGroupName} from "./queue-group-name";
 import {Message} from "node-nats-streaming";
+import {expirationQueue} from "../../queues/expiration-queue";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     subject: Subjects.OrderCreated = Subjects.OrderCreated;
     queueGroupName = queueGroupName;
 
-    onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+        const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
 
+        console.log('Waiting this many milliseconds to process the job:', delay);
+
+        await expirationQueue.add({
+            orderId: data.id
+        }, {
+            delay,
+        });
+
+        msg.ack();
     }
 
 
